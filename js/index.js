930be4c500e4c07d9f117363dd2bc33c01c4c7e9
Alexander Bychkov <alx.bychkov@gmail.com>
2022-02-15 14:54:40 +0300

    fix date

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
-const swiper = new Swiper('.swiper', {
-    loop: true,
-    slidesPerView: 1,
-    spaceBetween: 10,
-    autoplay: {
-        delay: 6000
-    },
-    grabCursor: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        1024: {
-            spaceBetween: 0
-        },
-        561: {
-            spaceBetween: 10
-        }
-    }
-  });
+// const swiper = new Swiper('.swiper', {
+//     loop: true,
+//     slidesPerView: 1,
+//     spaceBetween: 10,
+//     autoplay: {
+//         delay: 6000
+//     },
+//     grabCursor: true,
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//     breakpoints: {
+//         1024: {
+//             spaceBetween: 0
+//         },
+//         561: {
+//             spaceBetween: 10
+//         }
+//     }
+//   });
